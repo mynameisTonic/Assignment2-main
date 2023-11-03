a9a89f7435ea2835597e25ac0a28a160a1000316
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-03 05:05:33 -0600

    FINALLY

--- Assignment2-main/main.py
+++ Assignment2-main/main.py
@@ -26,7 +26,7 @@ down_payment_input = float(input(f"Enter down payment percentage (minimum {round
 
 down_payment_percent = down_payment_input/100
 down_payment_total = down_payment_percent * purchase
-print(f"Down payment total:{round(down_payment_total, 1)}")
+print(f"Down payment total: {round(down_payment_total, 1)}")
 
 #insurance calculation
 #premium insurance rates
@@ -63,31 +63,35 @@ else:
     print(f"Mortgage Insurance price is {insurance_cost}")
     print(f"Total mortgage amount is ${round(principal_amount, 1)}")
 
-intrest = 0
-
 #mortgage term calculations
 while True:
     mortgage_term = input("Enter mortgage term (1, 2, 3, 5, or 10): ")
     if mortgage_term == "1":
-        intrest_rate = intrest + 0.0595
+        intrest_rate = 0.0595
         break
     elif mortgage_term == "2":
-        intrest_rate = intrest + 0.059
+        intrest_rate = 0.059
         break
     elif mortgage_term == "3":
-        intrest_rate = intrest + 0.056
+        intrest_rate = 0.056
         break
     elif mortgage_term == "5":
-        intrest_rate = intrest + 0.0529
+        intrest_rate = 0.0529
         break
     elif mortgage_term == "10":
-        intrest_rate = intrest + 0.06
+        intrest_rate = 0.06
         break
     else:
         print("Please enter a valid choice")
 
+def calc_monthly_rate(interest_rate):
+    calc1 = (1 + interest_rate) /2
+    calc2 = calc1**2
+    calc3 = calc2**(1/12) - 1
+    return calc3
 
-effective_monthly_rate = (((1 + (intrest_rate) / 2 ) ** 2) ** (1/12) - 1)
+effective_monthly_rate = calc_monthly_rate(intrest_rate)
+
 # def effective_monthly_rate(interest_rate):
 #     one_plus_a = 1 + interest_rate
 #     power_of_two = one_plus_a ** 2
@@ -125,8 +129,19 @@ print(f"Intrest rate for the term will be {round(annual_intrest_rate, 3)}%")
 #num_payments = number of payments
 num_payments = int(mortgage_term) * 12
 
-emr_cluster = (effective_monthly_rate + 1) ** (num_payments)
-monthly_payment = principal_amount * (effective_monthly_rate * emr_cluster) / (emr_cluster - 1)
+def calculate_monthly_payment(num_payments, principal_amount, effective_monthly_rate):
+
+    calc1 = effective_monthly_rate**num_payments
+    calc2 = effective_monthly_rate * (1 + calc1)
+
+    calc3 = (1 + effective_monthly_rate)**num_payments
+    calc4 = calc3 - 1
+
+    full_calc = principal_amount * (calc2 / calc4)
+    return full_calc
+
+monthly_payment = calculate_monthly_payment(num_payments, principal_amount, effective_monthly_rate)
+
 print(f"Your monthly payments will be {round(monthly_payment, 2)} per month")
 
 schedule = input("Would you like to see the amortization schedule? (Y/N): ").lower()
